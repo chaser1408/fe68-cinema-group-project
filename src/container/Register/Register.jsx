d92acc7d32ab0e8b519358d0820d2cefa8094d9c
nguyenmya <80749352+nguyenmya@users.noreply.github.com>
2021-09-04 20:55:52 +0700

    push lên cho a ý lây về

--- src/container/Register/Register.jsx
+++ src/container/Register/Register.jsx
@@ -3,6 +3,53 @@ import React from 'react'
 export default function Register() {
     return (
         <div>
+              <form onSubmit="">
+            <li>
+                <input name="taikhoan"
+                onChange=""
+                type="text" className="text"
+                 defaultValue="taikhoan"
+                  />
+            </li>
+            <li>
+                <input name="matkhau" 
+                onChange=""
+                 type="password"
+                //  autoComplete="current-password"
+                 defaultValue="matkhau" />
+            </li>
+            <li>
+                <input name="email" 
+                onChange=""
+                 type="email"
+                //  autoComplete="current-password"
+                 placeholder="email" />
+            </li>
+            <li>
+                <input name="soDt" 
+                onChange=""
+                 type="text"
+                //  autoComplete="current-password"
+                 placeholder="soDt" />
+            </li> 
+             <li>
+                <input name="maNhom" 
+                onChange=""
+                 type="maNhom"
+                //  autoComplete="current-password"
+                 placeholder="maNhom" />
+            </li> 
+             <li>
+                <input name="hoTen" 
+                onChange=""
+                 type="text"
+                //  autoComplete="current-password"
+                 placeholder="hoTen" />
+            </li>
+            <div className="submit">
+                <button type="submit">DN</button>
+            </div>
+        </form>
             
         </div>
     )
